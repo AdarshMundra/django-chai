201bc3b3e60cd8c8ec80c59105aaa44256deafd4
Adarshmundra <adarsh.mundra1305@gmail.com>
2024-06-27 21:40:02 +0530

    relation and models

--- chaiaurdjango/chai/urls.py
+++ chaiaurdjango/chai/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
 
     path("",views.chai, name="chai"),
-    
+    path('chai_stores/', views.chai_store_view, name='chai_stores'),
     path("<int:chai_id>/",views.chai_details, name="chai_detail"),
 ]
