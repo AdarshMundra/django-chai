4d2096348af4b77eb914d4351b72a35c9865b127
adarshmundra <adarsh.mundra1305@gmail.com>
2024-06-29 23:47:40 +0530

    templates added

--- MegaProject/tweet/forms.py
+++ MegaProject/tweet/forms.py
@@ -1,7 +1,7 @@
-from django import forms
+from django.forms import ModelForm
 from .models import Tweet
 
-class Tweetform(forms.ModelForm):
+class Tweetform(ModelForm):
     class Meta:
         model = Tweet
         field = ['text','photo']
--- MegaProject/tweet/views.py
+++ MegaProject/tweet/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404, redirect
+
 from .models import Tweet
 from .forms import Tweetform
-from django.shortcuts import get_object_or_404, redirect
 
 
 # Create your views here.
